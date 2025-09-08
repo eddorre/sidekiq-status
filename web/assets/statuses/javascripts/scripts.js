f9e6675ab2bf4cc7d5bd29f825351493574c669d
Carlos Rodriguez <carlos@eddorre.com>
2025-09-08 11:45:45 -0700

    Ensure DOM is loaded before we attach event handlers

--- web/assets/statuses/javascripts/scripts.js
+++ web/assets/statuses/javascripts/scripts.js
@@ -1,5 +1,7 @@
-document.getElementById("sidekiq-status-select-status-name").addEventListener("change", handleSelectChange);
-document.getElementById("sidekiq-status-select-per-page").addEventListener("change", handleSelectChange);
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("sidekiq-status-select-status-name").addEventListener("change", handleSelectChange);
+  document.getElementById("sidekiq-status-select-per-page").addEventListener("change", handleSelectChange);
+});
 
 function handleSelectChange() {
   window.location = this.options[this.selectedIndex].getAttribute("data-url");
